98be5a2c009ff4a47ca0d214b061bd4f00dc11e1
Administrator <Administrator@BESBE>
2020-12-05 18:15:59 +0000

    Update Stone_Eater.lua
    Stone Eaters won't cast magic at melee range.

--- scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
+++ scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
@@ -8,3 +8,11 @@ require("scripts/globals/regimes")
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 77, 1, tpz.regime.type.FIELDS)
 end
+
+function onMobFight(mob, target)
+	if mob:checkDistance(target) < 3 then
+    mob:SetMagicCastingEnabled(false)
+	else
+	mob:SetMagicCastingEnabled(true)
+	end
+end 
