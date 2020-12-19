d4d972c15d0003d42276eb78b56e7bad078a61a8
Gibbz <Deron.gibson@yahoo.com>
2020-12-18 20:06:28 -0700

    Update bomb_toss_suicide.lua
    Reduced Dmg (less random)

--- scripts/globals/mobskills/bomb_toss_suicide.lua
+++ scripts/globals/mobskills/bomb_toss_suicide.lua
@@ -21,7 +21,7 @@ function onMobWeaponSkill(target, mob, skill)
     local BOMB_TOSS_HPP = skill:getMobHPP() / 100
 
     local job = mob:getMainJob()
-    local power = math.random(12, 18)
+    local power = math.random(12, 14)
 
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*power*BOMB_TOSS_HPP, tpz.magic.ele.FIRE, dmgmod, TP_MAB_BONUS, 1)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, tpz.attackType.MAGICAL, tpz.damageType.FIRE, MOBPARAM_IGNORE_SHADOWS)
