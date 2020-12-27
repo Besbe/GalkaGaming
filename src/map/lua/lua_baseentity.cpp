e2e1bebd41b0bea5a352f45585fc358f112ed292
Diego <recioszack@gmail.com>
2020-12-26 20:16:34 -0800

    Adding Guild weekday holidays

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -2307,17 +2307,16 @@ inline int32 CLuaBaseEntity::sendGuild(lua_State* L)
     TPZ_DEBUG_BREAK_IF(open > close);
 
     uint8 VanadielHour = (uint8)CVanaTime::getInstance()->getHour();
-    // uint8 VanadielDay = (uint8)CVanaTime::getInstance()->getWeekday();
+    uint8 VanadielDay = (uint8)CVanaTime::getInstance()->getWeekday();
 
     GUILDSTATUS status = GUILD_OPEN;
 
-    /*
-     * No more guild holidays since 2014
+    
     if (VanadielDay == holiday)
     {
-        status = GUILD_HOLYDAY;
+       status = GUILD_HOLYDAY;
     }
-    */
+    
     if ((VanadielHour < open) || (VanadielHour >= close))
     {
         status = GUILD_CLOSE;
