d7ee132173478df1c2b928a0a4c469e8cfccf5ab
Gibbz <Deron.gibson@yahoo.com>
2020-12-28 22:31:45 -0700

    Smithing Guild Merchant fix
    Fixed Smithing guiild merchant and cleaned up a few prices that were over 100k

--- scripts/zones/Metalworks/npcs/Vicious_Eye.lua
+++ scripts/zones/Metalworks/npcs/Vicious_Eye.lua
@@ -4,20 +4,18 @@
 -- Type: Guild Merchant (Blacksmithing Guild)
 -- !pos -106.132 0.999 -28.757 237
 -----------------------------------
-local ID = require("scripts/zones/Metalworks/IDs")
-require("scripts/globals/shop")
 require("scripts/globals/settings")
-require("scripts/globals/status")
+require("scripts/globals/shop")
+local ID = require("scripts/zones/Metalworks/IDs")
 -----------------------------------
 
 function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    local guildSkillId = tpz.skill.SMITHING
-    local stock = tpz.shop.generalGuildStock[guildSkillId]
-    tpz.shop.generalGuild(player, stock, guildSkillId)
-    player:showText(npc, ID.text.VICIOUS_EYE_SHOP_DIALOG)
+    if (player:sendGuild(5332, 8, 23, 2)) then
+        player:showText(npc, ID.text.VICIOUS_EYE_SHOP_DIALOG)
+    end
 end
 
 function onEventUpdate(player, csid, option)
