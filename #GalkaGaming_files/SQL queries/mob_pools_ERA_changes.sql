e45dfbef6a23f99f7c1cb64368a586199b0824fd
Diego Diaz <recioszack@gmail.com>
2020-12-30 20:57:58 -0800

    Adding couple more custom tables
    Custom family for Goblin Fishers and Qutrubs/Idolaters

---
+++ #GalkaGaming_files/SQL queries/mob_pools_ERA_changes.sql
@@ -0,0 +1,11 @@
+-- Doubling HP of Idolaters/Qutrubs
+-- IMPORTANT: Run mob_family_system script first!!!
+
+UPDATE mob_pools SET familyid = "96" WHERE name = "Qutrub";
+UPDATE mob_pools SET familyid = "96" WHERE name = "Qutrub_Extortionist";
+UPDATE mob_pools SET familyid = "96" WHERE name = "Qutrub_Forayer";
+UPDATE mob_pools SET familyid = "96" WHERE name = "Qutrub_Wastrel";
+UPDATE mob_pools SET familyid = "96" WHERE name = "Lamia_Idolater"; 
+
+
+
