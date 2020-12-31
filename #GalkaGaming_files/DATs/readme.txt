6e20065642d28823fc3dfa35bb1b085253b3e81e
Diego Diaz <recioszack@gmail.com>
2020-12-30 19:07:51 -0800

    Custom GalkaGaming files
    SQL queries to be run and DAT files for clients.

---
+++ #GalkaGaming_files/DATs/readme.txt
@@ -0,0 +1,8 @@
+--- Updates class list on spells to ERA (e.g: removes RUN, GEO and such from all spells ---
+ROM\118\107.DAT
+
+--- Updates cast bar timing to ERA ---
+ROM\118\114.DAT
+
+-- Updates Merit Group2 to ERA ---
+ROM\147\118.DAT
---
+++ #GalkaGaming_files/SQL queries/traits_fix.sql
@@ -0,0 +1,17 @@
+-- Fixes issue where non-merited job traits appear under "Abilities/Job Traits" when you reach 75
+
+UPDATE traits SET meritid = "2116" WHERE name = "invigorate";
+UPDATE traits SET meritid = "2118" WHERE name = "penance";
+UPDATE traits SET meritid = "2564" WHERE name = "beast affinity";
+UPDATE traits SET meritid = "2566" WHERE name = "beast healer";
+UPDATE traits SET meritid = "2692" WHERE name = "snapshot";
+UPDATE traits SET meritid = "2694" WHERE name = "recycle";
+UPDATE traits SET meritid = "2884" WHERE name = "empathy";
+UPDATE traits SET meritid = "2886" WHERE name = "strafe";
+UPDATE traits SET meritid = "3012" WHERE name = "enchainment";
+UPDATE traits SET meritid = "3014" WHERE name = "assimilation";
+UPDATE traits SET meritid = "3076" WHERE name = "winning streak";
+UPDATE traits SET meritid = "3078" WHERE name = "loaded deck";
+UPDATE traits SET meritid = "3140" WHERE name = "fine-tuning";
+UPDATE traits SET meritid = "3274" WHERE name = "stormsurge";
+
